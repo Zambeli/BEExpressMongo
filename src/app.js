ac0c2de15075feb626391546ab3d6333b75101f7
Gustavo Zambeli <123279272+Zambeli@users.noreply.github.com>
2024-11-21 15:43:39 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,52 +1,19 @@
-import express, { json, response } from "express";
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexao", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+});
 
 const app = express();
-
-app.use(express.json());
-
-const livros = [
-  {
-    id: 1,
-    titulo: "A divina comédia",
-  },
-  {
-    id: 2,
-    titulo: "A corte",
-  },
-];
-
-function buscaLivro(id) {
-  return livros.findIndex((livro) => {
-    return livro.id === Number(id);
-  });
-}
-
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-
-  res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("Livro cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-
-  livros[index].titulo = req.body.titulo;
-
-  res.status(200).json(livros);
-});
+routes(app);
 
 app.delete("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
